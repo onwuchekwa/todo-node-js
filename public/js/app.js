2d4d62414f74cde2dde81b446eab21eaf2956ee3
onwuchekwa <donsonde@gmail.com>
2020-12-01 08:28:46 +0000

    changes in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -2,45 +2,45 @@ $(document).ready(function(){
     $('#addTask').on('submit', function() {
         window.location.reload();
     });
-
-    const toggleTaskStatus = (event, id) => {
-        const{name} = event.target;
-
-        fetch('/users/dashboard', {
-            method: 'PUT',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                name,
-                id
-            })
-        })
-        .then(response => response.json())
-        .then(status => {
-            console.log(status);
-            window.location.reload();
-        })
-        .catch(err => console.log(err))
-    }
-
-    const deleteTask = (event, id) => {
-        const{name} = event.target;
-        fetch('/users/dashboard', {
-            method: 'DELETE',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                name,
-                id
-            })
-        })
-        .then(response => response.json())
-        .then(status => {
-            console.log(status);
-            window.location.reload();
-        })
-        .catch(err => console.log(err))
-    }
 });
+
+const toggleTaskStatus = (event, id) => {
+    const{name} = event.target;
+
+    fetch('/users/dashboard', {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            id
+        })
+    })
+    .then(response => response.json())
+    .then(status => {
+        console.log(status);
+        window.location.reload();
+    })
+    .catch(err => console.log(err))
+}
+
+const deleteTask = (event, id) => {
+    const{name} = event.target;
+    fetch('/users/dashboard', {
+        method: 'DELETE',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            id
+        })
+    })
+    .then(response => response.json())
+    .then(status => {
+        console.log(status);
+        window.location.reload();
+    })
+    .catch(err => console.log(err))
+}
