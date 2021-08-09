dc272d2963476fff2844b3100eec17f2c215c281
Sunday Onwuchekwa <donsonde@gmail.com>
2021-08-09 09:11:23 +0000

    DbConfig file modified

--- dbConfig.js
+++ dbConfig.js
@@ -5,7 +5,10 @@ const{ Pool } = require('pg');
 const connectionString = process.env.DATABASE_URL || `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const pool = new Pool({
-    connectionString: connectionString
+    connectionString: connectionString,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 });
 
 module.exports = { pool };
